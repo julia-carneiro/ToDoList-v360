5cbfe2144218a2c90a0353f5923444237dc34f55
Júlia Carneiro <jcgsouza@ecomp.uefs.br>
2025-01-12 02:18:04 -0300

    add pagination to tasks and lists
    css still not 100%

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,7 +3,7 @@ class ListsController < ApplicationController
 
   # GET /lists or /lists.json
   def index
-    @lists = current_user.lists
+    @lists = current_user.lists.page(params[:page]).per(8)
   end
 
   # GET /lists/1 or /lists/1.json

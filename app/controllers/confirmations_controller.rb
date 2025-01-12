902cc5c8be87645b7a6633c152e2aa7df4cb85a8
Júlia Carneiro <jcgsouza@ecomp.uefs.br>
2025-01-11 23:07:46 -0300

    add username to e-mail and list page

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -4,7 +4,7 @@ class ConfirmationsController < ApplicationController
     user = User.find_by(confirmation_token: params[:token])
     if user
       user.confirm!
-      redirect_to new_list_path, notice: "Your email has been confirmed. Please log in."
+      redirect_to new_session_path, notice: "Your email has been confirmed. Please log in."
     else
       flash[:alert] = "Invalid or already confirmed token."
       redirect_to root_path
